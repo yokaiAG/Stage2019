313900324edd826d7904747e311175d7c9180c46
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-04-16 14:22:38 +0200

    sort_psimodel+2playerexactsol

---
+++ Code/sort_psimodel.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import sys
+from operator import itemgetter
+
+infile = str(sys.argv[1])
+outfile = str(sys.argv[2])
+
+# load psis
+psi_model = list()
+for line in open(infile):
+    line = line.split()
+    user, psi = int(line[0]), float(line[1])
+    psi_model.append((user, psi))
+
+# sort
+psi_model = sorted(psi_model, key=itemgetter(1), reverse=True)
+
+# write to out
+out = open(outfile, 'w')
+for user, psi in psi_model:
+    out.write("{} {}".format(user, psi))
+out.close()

9a3c30be6303b45c4f5702cc2cebc902c48b4418
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-05-17 12:19:45 +0200

    count rt vs real edges

--- Code/countRT_vs_realEdges.py
+++ Code/countRT_vs_realEdges.py
@@ -13,7 +13,7 @@ Author = util.get_authors(data_path)
 countRT = dict()
 for line in open(data_path):
     line = line.split()
-    uid, rtid = line[2], line[-1]
+    uid, rtid = int(line[2]), int(line[-1])
     if rtid != -1 and rtid in Author:
         rtu = Author[rtid]
         if (uid, rtu) in countRT:
@@ -27,18 +27,18 @@ countRT_vs_real = list()
 real_edges = set()
 for line in open(adjList_path):
     line = line.split()
-    l, f = line[0], line[1]
-    if (l,f) in countRT:
-        real_edges.add((l,f))
-        countRT_vs_real.append((countRT[(l,f)], 1))
+    l, f = int(line[0]), int(line[1])
+    if (f,l) in countRT:
+        real_edges.add((f,l))
+        countRT_vs_real.append((countRT[(f,l)], 1))
     else:
         countRT_vs_real.append((0,1))
 for e in countRT:
     if e not in real_edges:
-        countRT_vs_real.append((countRT[(l,f)], 0))
+        countRT_vs_real.append((countRT[e], 0))
 
 # compare
-plt.scatter([x[0] for x in countRT_vs_real], [x[0] for x in countRT_vs_real])
-plt.savefig(out_path + "count rt vs real edges")
-plt.show()
-plt.close()
+out = open("count rt vs real edges.txt", "w")
+for nbrt, real in countRT_vs_real:
+    out.write(str(nbrt) + " " + str(real) + "\n")
+out.close()

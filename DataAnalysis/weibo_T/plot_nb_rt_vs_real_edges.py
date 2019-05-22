2a722ca55c16906407ed1a303eebde25dfbf88c6
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-05-22 18:25:55 +0200

    plot_nb_rt_vs_real_edges.py up

--- DataAnalysis/weibo_T/plot_nb_rt_vs_real_edges.py
+++ DataAnalysis/weibo_T/plot_nb_rt_vs_real_edges.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from operator import itemgetter
 
+print("Reading input file...")
 rt2edges = dict()
 for line in open("nb_rt_vs_real_edges.txt"):
     line = line.split()
@@ -16,12 +17,17 @@ for line in open("nb_rt_vs_real_edges.txt"):
         else:
             rt2edges[rt][0] += 1
 
+print("Sorting results for plot...")
 x2plot = sorted(rt2edges.keys())
 y2plot = [y[1] for y in sorted(rt2edges.items(), key=itemgetter(0))]
 y2plot = [ y[0]/sum(y) for y in y2plot ]
+
+print("Plotting...")
 plt.plot(x2plot, y2plot, marker='x')
 plt.xlabel("nb rt")
 plt.ylabel("edge proba")
 plt.savefig("rt_vs_real_edges_plot.png")
 plt.show()
 plt.close()
+
+print("Done !")

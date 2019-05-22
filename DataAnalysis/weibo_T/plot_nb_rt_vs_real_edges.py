3e69980d84831637d4fa593d7c1a72296b9eb5e8
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-05-22 12:48:34 +0200

    Newman_sampleTest.ipynb up and nb rt vs real edges .py

---
+++ DataAnalysis/weibo_T/plot_nb_rt_vs_real_edges.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from operator import itemgetter
+
+rt2edges = dict()
+for line in open("nb_rt_vs_real_edges.txt"):
+    line = line.split()
+    rt, edge = int(line[0]), int(line[1])
+    if rt not in rt2edges:
+        if edge==0:
+            rt2edges[rt] = [0,1]
+        else:
+            rt2edges[rt] = [1,0]
+    else:
+        if edge==0:
+            rt2edges[rt][1] += 1
+        else:
+            rt2edges[rt][0] += 1
+
+x2plot = sorted(rt2edges.keys())
+y2plot = sorted(rt2edges.items(), key=itemgetter(1))
+y2plot = [ y/sum(y) for y in y2plot ]
+plt.plot(x2plot, y2plot)
+plt.xlabel("nb rt")
+plt.ylabel("edge proba")
+plt.savefig("rt_vs_real_edges_plot.pdf")
+plt.show()
+plt.close()

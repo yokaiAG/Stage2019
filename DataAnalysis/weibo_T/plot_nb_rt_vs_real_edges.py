6f056ae290b7413d05e0ac09bb555296740e8aab
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-05-22 17:47:23 +0200

    newman simulator nbrtvsreal up

--- DataAnalysis/weibo_T/plot_nb_rt_vs_real_edges.py
+++ DataAnalysis/weibo_T/plot_nb_rt_vs_real_edges.py
@@ -17,7 +17,7 @@ for line in open("nb_rt_vs_real_edges.txt"):
             rt2edges[rt][0] += 1
 
 x2plot = sorted(rt2edges.keys())
-y2plot = sorted(rt2edges.items(), key=itemgetter(1))
+y2plot = sorted(rt2edges.items(), key=itemgetter(0))[1]
 y2plot = [ y[0]/sum(y) for y in y2plot ]
 plt.plot(x2plot, y2plot)
 plt.xlabel("nb rt")

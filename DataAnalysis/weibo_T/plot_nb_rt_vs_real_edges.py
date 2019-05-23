f3f83ce0c67131c3e5d2aa60daf3f4d8358f0a22
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-05-23 10:40:10 +0200

    plot_nb_rt_vs_real_edges.py up

--- DataAnalysis/weibo_T/plot_nb_rt_vs_real_edges.py
+++ DataAnalysis/weibo_T/plot_nb_rt_vs_real_edges.py
@@ -22,11 +22,19 @@ x2plot = sorted(rt2edges.keys())
 y2plot = [y[1] for y in sorted(rt2edges.items(), key=itemgetter(0))]
 y2plot = [ y[0]/sum(y) for y in y2plot ]
 
-print("Plotting...")
-plt.plot(x2plot, y2plot, marker='x')
+print("Scatter plot...")
+plt.scatter(x2plot, y2plot, alpha=.5, marker='x')
 plt.xlabel("nb rt")
 plt.ylabel("edge proba")
-plt.savefig("rt_vs_real_edges_plot.png")
+plt.savefig("nbRT_vs_realEdges_scatter.png")
+plt.show()
+plt.close()
+
+print("Histogram...")
+plt.hist(x2plot, y2plot, facecolor='yellow', edgecolor='red')
+plt.xlabel("nb rt")
+plt.ylabel("edge proba")
+plt.savefig("nbRT_vs_realEdges_hist.png")
 plt.show()
 plt.close()
 

c6e07cd66d0735cd6487311ffa41a744bd803fb2
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-06-05 17:03:15 +0200

    subGraphAndTrace_creation.py

---
+++ Code/subGraphAndTrace_creation.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+import sys
+import util
+from operator import itemgetter
+
+# init
+adjlist_path = str(sys.argv[1])
+trace_path = str(sys.argv[2])
+
+# import graph 
+LeadGraph, FollowGraph = graph_from_adjList(adjlist_path)
+
+# get most followed user and relations among her followers
+most_influential_user = max(((u, len(FollowGraph[u])) for u in Followgraph), key=itemgetter(1))
+print(most_influential_user)

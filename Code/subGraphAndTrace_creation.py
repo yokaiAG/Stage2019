eaa0d375dccd91aedbcf0cce27a35db167d57109
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-06-05 18:30:23 +0200

    subGraphAndTrace_creation.py

--- Code/subGraphAndTrace_creation.py
+++ Code/subGraphAndTrace_creation.py
@@ -7,17 +7,43 @@ from operator import itemgetter
 # init
 adjlist_path = str(sys.argv[1])
 trace_path = str(sys.argv[2])
-nb_top_users = int(sys.argv[3])
+out_path = str(sys.argv[3])
+max_nb_follow = int(sys.argv[4])
 
 # import graph 
 print("Importing user graph...")
 LeadGraph, FollowGraph = util.graph_from_adjList(adjlist_path)
+del LeadGraph
 
-# get most followed user and relations among her followers
+# get most followed user among those with <1000 followers
 print("Searching for most followed users...")
-i = 0
 for (user, nb_follow) in sorted(((u, len(FollowGraph[u])) for u in FollowGraph), key=itemgetter(1), reverse=True):
-    print(user, nb_follow)
-    i += 1
-    if i==nb_top_users:
+    if nb_follow < max_nb_follow:
         break
+FollowGraph = {u: FollowGraph[u] for u in FollowGraph[user]}
+
+# write adjlist among her and her followers
+print("writing new adjacency list...")
+out = open(out_path + "sub_adjlist.txt", "w")
+for u in FollowGraph[user]:
+    out.write("{} {}\n".format(user, u))
+    for v in FollowGraph[u]:
+        if v in FollowGraph[user]:
+            out.write("{} {}\n".format(u, v))
+out.close()
+    
+# write new trace
+print("writing new twitter trace...")
+Author = util.get_authors(trace_path)
+out = open("out_path" + "sub_trace.txt", "w")
+for line in open(trace_path):
+    line = line.split()
+    uid, rtid = int(line[2]), int(line[3])
+    if uid in Author:
+        if rtid not in Author: # covers the case rtid=-1
+            out.write(line)
+        elif Author[rtid] in FollowGraph:
+            out.write(line)
+out.close()
+
+print("done!")

fc585dcfb543729339b87b84d2b3e71808c6783a
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-06-05 17:15:55 +0200

    subGraphAndTrace_creation.py

--- Code/subGraphAndTrace_creation.py
+++ Code/subGraphAndTrace_creation.py
@@ -9,8 +9,10 @@ adjlist_path = str(sys.argv[1])
 trace_path = str(sys.argv[2])
 
 # import graph 
+print("Importing user graph...")
 LeadGraph, FollowGraph = util.graph_from_adjList(adjlist_path)
 
 # get most followed user and relations among her followers
-most_influential_user = max(((u, len(FollowGraph[u])) for u in FollowGraph), key=itemgetter(1))
-print(most_influential_user)
+print("Searching for most followed user...")
+most_followed_user = max(((u, len(FollowGraph[u])) for u in FollowGraph), key=itemgetter(1))
+print("Most followed user and her number of followers : ", most_followed_user)

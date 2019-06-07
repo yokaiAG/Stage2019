ab53bf604873528c136c65cf7520f143514893d8
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-06-07 16:33:36 +0200

    Newman_scalable_adaptiveW.ipynb + subGraphAndTrace_creation.py UP

--- Code/subGraphAndTrace_creation.py
+++ Code/subGraphAndTrace_creation.py
@@ -16,37 +16,44 @@ LeadGraph, FollowGraph = util.graph_from_adjList(adjlist_path)
 del LeadGraph
 
 # get most followed user among those with <1000 followers
-print("Searching for most followed users...")
+print("Searching for a central user...")
 for (user, nb_follow) in sorted(((u, len(FollowGraph[u])) for u in FollowGraph), key=itemgetter(1), reverse=True):
     if nb_follow < max_nb_follow:
         break
-FollowGraph = {u: FollowGraph[u] for u in set(FollowGraph[user]).union({user})}
+users = set(FollowGraph[user]).union({user})
 
-# write adjlist among her and her followers
-print("Writing new adjacency list...")
-out = open(out_path + "subAdjlist.txt", "w")
-for u in FollowGraph[user]:
-    out.write("{} {}\n".format(user, u))
-    for v in FollowGraph[u]:
-        if v in FollowGraph[user]:
-            out.write("{} {}\n".format(u, v))
-out.close()
-    
-# write new trace
+# write new trace and delete users that are not present in the trace
 print("Getting authors for original twitter trace...")
 Author = util.get_authors(trace_path)
 
 print("Writing new twitter trace...")
+trace_users = set()
 out = open(out_path + "subTrace.txt", "w")
 for line in open(trace_path):
     line_ = line.split()
     uid, rtid = int(line_[2]), int(line_[3])
-    if uid in FollowGraph:
+    if uid in users:
+        trace_users.add(uid)
         if rtid == -1:
             out.write(line)
         elif rtid in Author:
-            if Author[rtid] in FollowGraph:
+            if Author[rtid] in users:
                 out.write(line)
+                trace_users.add(Author[rtid])
 out.close()
 
+print("Restricting to active users...")
+users = set(trace_users)
+del trace_users
+
+# write adjlist among her and her followers
+print("Writing new adjacency list...")
+out = open(out_path + "subAdjlist.txt", "w")
+for u in users:
+    for v in FollowGraph[u]:
+        if v in users:
+            out.write("{} {}\n".format(u, v))
+out.close()
+
+# end
 print("done!")

1378aa4edebec51bf12d741237c0af221a19a61b
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-06-07 17:17:17 +0200

    subGraphAndTrace_creation.py UP

--- Code/subGraphAndTrace_creation.py
+++ Code/subGraphAndTrace_creation.py
@@ -29,6 +29,7 @@ Author = util.get_authors(trace_path)
 print("Writing new twitter trace...")
 trace_users = set()
 out = open(out_path + "subTrace.txt", "w")
+nb_lines = 0
 for line in open(trace_path):
     line_ = line.split()
     uid, rtid = int(line_[2]), int(line_[3])
@@ -36,11 +37,14 @@ for line in open(trace_path):
         trace_users.add(uid)
         if rtid == -1:
             out.write(line)
+            nb_lines += 1
         elif rtid in Author:
             if Author[rtid] in users:
                 out.write(line)
+                nb_lines += 1
                 trace_users.add(Author[rtid])
 out.close()
+print("Nb lines in new trace : {}".format(nb_lines))
 
 print("Restricting to active users...")
 users = set(trace_users)
@@ -49,11 +53,14 @@ del trace_users
 # write adjlist among her and her followers
 print("Writing new adjacency list...")
 out = open(out_path + "subAdjlist.txt", "w")
+nb_lines = 0
 for u in users:
     for v in FollowGraph[u]:
         if v in users:
             out.write("{} {}\n".format(u, v))
+            nb_lines += 1
 out.close()
+print("Nb lines in new adjlist : {}".format(nb_lines))
 
 # end
-print("done!")
+print("\nFinish !")

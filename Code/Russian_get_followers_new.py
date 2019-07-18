f3e0dcc2c1a5bc2d2f146f7c55e8beb914fc6c50
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-07-18 19:04:07 +0200

    up

--- Code/Russian_get_followers_new.py
+++ Code/Russian_get_followers_new.py
@@ -16,13 +16,13 @@ OAUTH_TOKEN_SECRET = "iiD75H9ERtkATnd74pVRBfM7TI189BSmDasYzN2uUO123"
 
 # Get user set from rtu data.
 print("Getting users and errors...")
-user2error = dict()
+user2cursor = dict()
 for line in open("../Datasets/russian_getfollowers_error_logs.txt")
     u, err = int(line.split()[0]), int(line.split()[1])
-    if u in user2error:
-        user2error[u].add(err)
+    if u in user2cursor:
+        user2cursor[u].add(err)
     else:
-        user2error[u] = err
+        user2cursor[u] = err
 
 # init
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
@@ -31,7 +31,7 @@ outfile = "../Datasets/russian_adjList_new.txt"
 errorfile = "../Datasets/russian_errors_new.txt"
 
 # iterate
-for u,cursor in user2error.items():
+for u,cursor in user2cursor.items():
         
     try:
         # get followers

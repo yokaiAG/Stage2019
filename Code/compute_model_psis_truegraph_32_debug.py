742c7dcea9f48427ca25399d590f20df5d83fb29
Antoine Vendeville <antoine.vendeville@hotmail.fr>
2019-07-19 16:24:27 +0200

    up

--- Code/compute_model_psis_truegraph_32_debug.py
+++ Code/compute_model_psis_truegraph_32_debug.py
@@ -25,9 +25,12 @@ import sys
 from numba import jit
 from time import time
 
+# for debugging
+debug = open("debugging_weibo.txt", "w")
+
 
 # INIT
-print("init...")
+debug.write("\n"); debug.write("init...")
 data_path = str(sys.argv[1])
 adjList_path = str(sys.argv[2])
 cascade = bool(int(sys.argv[3]))
@@ -36,7 +39,7 @@ ibegin = int(sys.argv[5])
 iend = int(sys.argv[6])
 out_path = str(sys.argv[7])
 
-print("Cascade : {}".format(cascade))
+debug.write("\n"); debug.write("Cascade : {}".format(cascade))
 
 # to compute psi only for the nb_best most influent users from emul
 best_from_emul = bool(int(sys.argv[8]))
@@ -56,27 +59,27 @@ if best_from_emul:
 
 # Author dict creation if not RTU
 if cascade:
-    print("LastPublisher dict creation...")
+    debug.write("\n"); debug.write("LastPublisher dict creation...")
 else:
-    print("Author dict creation...")
+    debug.write("\n"); debug.write("Author dict creation...")
 Author = util.get_authors(data_path)
 
 
 # GET LAMBDAS MUS 
-print("Getting lambdas and mus...")
+debug.write("\n"); debug.write("Getting lambdas and mus...")
 start = time()
 Rtweet, Rrtweet, total_time = util.get_activity(data_path, cascade, Author, divide_by_time=True, retweeted=False)
-print("Done in {:.3f}s.".format(time()-start))
+debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
 
 # get leaders followers
-print("Getting leaders and followers...")
+debug.write("\n"); debug.write("Getting leaders and followers...")
 start = time()
 LeadGraph, FollowGraph = util.graph_from_adjList(adjList_path)
-print("Done in {:.3f}s.".format(time()-start))
+debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
 
 # get common users between trace and real graph
 # we remove users with 0 activity or 0 edges
-print("Searching for common users...")
+debug.write("\n"); debug.write("Searching for common users...")
 start = time()
 common_users_old = set(Rtweet.keys()).intersection(set(LeadGraph.keys()))
 common_users = set()
@@ -88,17 +91,17 @@ for u in common_users_old:
     else:
         common_users.add(u)
 del common_users_old
-print("Done in {:.3f}s.".format(time()-start))
+debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
 
 # eliminate users that are not common between graph and twitter trace
-print("Filtering common users...")
+debug.write("\n"); debug.write("Filtering common users...")
 start = time()
 Rtweet = { u: Rtweet[u] for u in common_users }
 Rrtweet = { u: Rrtweet[u] for u in common_users }
 LeadGraph = { u: LeadGraph[u].intersection(common_users) for u in common_users }
 FollowGraph = { u: FollowGraph[u].intersection(common_users) for u in common_users }
 del common_users
-print("Done in {:.3f}s.".format(time()-start))
+debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
 
 # list of users
 Lusers = list(Rtweet.keys())
@@ -197,7 +200,10 @@ def fill_di_sparse_v2(useri,Lvec,Mvec):
 def pi_method_sparse_v2(N,useri,A,A_trans,Lvec,Lead,Follow,Som,iter_infos, it = 1000, eps = .001):
     # v2: This method resolves the fixed-point exploiting vector sparsity.
     #
+    debug.write("\n"); debug.write("fill bi sparse...")
+    start = time()
     bi = fill_bi_sparse_v2(useri,Lvec,Som,Follow)
+    debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
     #
     # Initialisation (the result should be independent of initialisation vector)
     #
@@ -208,45 +214,58 @@ def pi_method_sparse_v2(N,useri,A,A_trans,Lvec,Lead,Follow,Som,iter_infos, it = 
     #
     t = int(0)
     while (t<it) & (normdiff>eps):
+        debug.write("\n"); debug.write("iteration #{}...".format(t+1))
         normdiff = np.float32(0)
         p_old = p_new.copy()
         p_new = {}
         # We search the lines of A which contain non-zero entries coinciding with the non-zero
         # entries of p_old.
+        debug.write("\n"); debug.write("mlines...")
+        start = time()
         mlines = set()
         for key in p_old:
             for tutu in A_trans[key]:
                 mlines.add(tutu)
+        debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
             #mlines = mlines.union(set(A_trans[key].keys()))
-        #print("p_old",p_old)
+        #debug.write("\n"); debug.write("p_old",p_old)
+        debug.write("\n"); debug.write("tutu...")
+        start = time()
         for tutu in bi:
             mlines.add(tutu)
+        debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
         #mlines = mlines.union(set(bi.keys()))
-        #print("mlines",mlines)
+        #debug.write("\n"); debug.write("mlines",mlines)
         for user in mlines:
             p_new[user] = np.float32(0)
+            debug.write("\n"); debug.write("for leader in Lead[user]...")
+            start = time()
             for leader in Lead[user]:
                 if leader in p_old:
                     p_new[user] += np.float32(A[user][leader]*p_old[leader])
             if user in bi.keys():
                 p_new[user]+=np.float32(bi[user])
+            debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
             # Norm 1 criterion:
             #normdiff += abs(p_old[user]-p_new[user])
             # Norm INF criterion:
+            debug.write("\n"); debug.write("if user in p_old keys...")
+            start = time()
             if user in p_old.keys():
                 if abs(p_old[user]-p_new[user])>normdiff:
                     normdiff = np.float32(abs(p_old[user]-p_new[user]))
             else:
                 if abs(0-p_new[user])>normdiff:
                     normdiff = np.float32(abs(0-p_new[user]))
+            debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
         t += 1
         #Tracer()()
-        #print("p_new",p_new)
+        #debug.write("\n"); debug.write("p_new",p_new)
     iter_infos.write("user {}: nb iter {}\n".format(useri,t))
     iter_infos.flush()
     #
-    # print("t=",t,"\n")
-    # print("diff_last=",normdiff,"\n")
+    # debug.write("\n"); debug.write("t=",t,"\n")
+    # debug.write("\n"); debug.write("diff_last=",normdiff,"\n")
     return p_new
 
 
@@ -271,23 +290,37 @@ def solution_sparse_v2(N,A,A_trans,C,Lvec,Mvec,Lead,Follow,Som,begin,end,fp,fq,f
                 continue
         sys.stdout.flush()
         sys.stdout.write("Computing p,q,Psi for user {} / {}...\r".format(l+1, end-begin))
+        debug.write("\n"); debug.write("pi method sparse...")
+        start = time()
         pNews[user] = pi_method_sparse_v2(N,user,A,A_trans,Lvec,Lead,Follow,Som,iter_infos)
+        debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
         #
+        debug.write("\n"); debug.write("fill di sparse...")
+        start = time()
         di = fill_di_sparse_v2(user,Lvec,Mvec)
+        debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
         qWall[user]=dict()
         Psi[user] = np.float32(0)
+        debug.write("\n"); debug.write("for userj in pNews[user]...")
+        start = time()
         for userj in pNews[user]:
             qWall[user][userj] = np.float32(C[userj]*pNews[user][userj])
             if userj==user:
                 qWall[user][userj]+=np.float32(di)
             Psi[user] += np.float32(qWall[user][userj])
+        debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
+        debug.write("\n"); debug.write("if user not in pNews[user]...")
+        start = time()
         if user not in pNews[user]:
             qWall[user][user] = np.float32(di)
             Psi[user] += np.float32(qWall[user][user])
         Psi[user] = np.float32((Psi[user]-qWall[user][user])/(N-1))
+        debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
         #if command can be used to break the routine at l==1000 or some other number.
         #if l == 1000:
         #    return (pNews,qWall,Psi)
+        debug.write("\n"); debug.write("save news wall...")
+        start = time()
         if save_news_wall:
             fp.write("%d "%user)
             for news in pNews[user]:
@@ -302,30 +335,31 @@ def solution_sparse_v2(N,A,A_trans,C,Lvec,Mvec,Lead,Follow,Som,begin,end,fp,fq,f
         fpsi.write("%d %g\n"%(user,Psi[user]))
         fpsi.flush()
         l += 1 # up counter
+        debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
     #
     return (pNews,qWall,Psi)
 
 
 # Calculation of the general input: dictionary Som and the three dictionaries A, A-trans, C for the matrices.
-print("Computing Som...")
+debug.write("\n"); debug.write("Computing Som...")
 start = time()
 Som = som_sparse(Rtweet,Rrtweet,LeadGraph)
-print("Done in {:.3f}s.".format(time()-start))
+debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
 
-print("Computing A...")
+debug.write("\n"); debug.write("Computing A...")
 start = time()
 A = fill_A_sparse(Rtweet,Rrtweet,LeadGraph,Som)
-print("Done in {:.3f}s.".format(time()-start))
+debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
 
-print("Computing A_trans...")
+debug.write("\n"); debug.write("Computing A_trans...")
 start = time()
 A_trans = fill_A_trans_sparse(Rtweet,Rrtweet,LeadGraph,Som)
-print("Done in {:.3f}s.".format(time()-start))
+debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
 
-print("Computing C...")
+debug.write("\n"); debug.write("Computing C...")
 start = time()
 C = fill_C_sparse(Rtweet,Rrtweet)
-print("Done in {:.3f}s.".format(time()-start))
+debug.write("\n"); debug.write("Done in {:.3f}s.".format(time()-start))
 
 # This routine just calculates the influence of a specific user on the Wall and Newsfeed of others as well as its Influence metric \Psi[user].
 @jit
@@ -350,7 +384,7 @@ def user_influence_v2(user,N,A,A_trans,C,Lvec,Mvec,Lead,Follow,Som,it = 100, eps
 
 
 # COMPUTE VALUES
-print("Computing p, q and psi...")
+debug.write("\n"); debug.write("Computing p, q and psi...")
 if iend==-1:
     iend = N
 
@@ -366,4 +400,4 @@ fq.close()
 fp.close()
 iter_infos.close()
 
-print("\nSuccess !")
+debug.write("\n"); debug.write("\nSuccess !")
